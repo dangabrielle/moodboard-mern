2aba3e96617b9399511c5fc29d01a1dfa841d5ca
dangabrielle <hoangabrielle@gmail.com>
2023-06-06 11:58:42 -1000

    add route/ctrl for openai API

--- server/server.js
+++ server/server.js
@@ -3,15 +3,22 @@ const path = require("path");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+
 require("dotenv").config();
 require("./config/database");
 app.use(express.json()); // Creates req.body
 app.use(morgan("dev"));
 app.use(cors());
+
+app.get("/", async (req, res) => {
+  res.send("Test from Express");
+});
+
+app.use("/api/v1/dalle", require("./routes/dalle"));
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-
 const port = process.env.PORT || 3001;
 app.listen(port, function () {
   console.log(`Express app is running on port:${port}`);
