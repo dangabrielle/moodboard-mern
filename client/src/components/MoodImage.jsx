53ef06e74f20124d80e744bf238328f6a20f7792
dangabrielle <hoangabrielle@gmail.com>
2023-06-08 08:36:33 -1000

    add moodImage component

--- client/src/components/MoodImage.jsx
+++ client/src/components/MoodImage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const MoodImage = () => {
-  return <div>MoodImage</div>;
+const MoodImage = ({ data }) => {
+  const { name, prompt, photo } = data;
+
+  return (
+    <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card p-0">
+      <img className="w-full h-auto object-cover rounded-xl" src={photo} />
+    </div>
+  );
 };
 
 export default MoodImage;
