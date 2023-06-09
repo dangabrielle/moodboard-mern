f18567dc1c3352a6d8d52ee0d54937f0d2b3df74
dangabrielle <hoangabrielle@gmail.com>
2023-06-08 21:25:13 -1000

    implement hover effect functionality in image comp

--- client/src/components/MoodImage.jsx
+++ client/src/components/MoodImage.jsx
@@ -4,11 +4,15 @@ const MoodImage = ({ data }) => {
   const { name, prompt, photo } = data;
 
   return (
-    <div className="rounded-xl group relative shadow-2xl shadow-card hover:shadow-cardhover card p-0">
+    <div className="rounded-xl h-full group relative shadow-2xl shadow-card hover:shadow-cardhover card p-0 ">
       <img
-        className="w-full h-auto object-cover shadow-2xl rounded-xl border-4 border-blue-50"
+        className="w-full relative h-full object-cover rounded-xl border-4 border-blue-50 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]"
         src={photo}
       />
+      <div className="group-hover:flex max-h-[94.5%] flex-col hidden absolute bottom-0 left-0 right-0 bg-[#6b7280a7] m-2 p-4 rounded-md">
+        <p className="text-white text-sm  ">{prompt}</p>
+        <p className="text-white text-sm  ">{name}</p>
+      </div>
     </div>
   );
 };
